9de70f3b2a437c3cc45fd8d73c9e7431bc1ba8fc
java2019 <java_2019@mail.ru>
2017-07-12 11:12:47 +0300

    first

--- app/src/main/java/com/nemo/cameraexample/MainActivity.java
+++ app/src/main/java/com/nemo/cameraexample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.nemo.cameraexample;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void NewPhoto() {
-        
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
